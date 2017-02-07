63dca2a00ebda433ac8cbd955e1b4c37385562d8
Edward Park <ed.sh.park@gmail.com>
2017-02-06 22:43:10 -0800

    created requestStream

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+// https://gist.github.com/staltz/868e7e9bc2a7b8c1f754
+// http://jsfiddle.net/staltz/8jFJH/48/
+
 let requestStream = Rx.Observable.just('https://api.github.com/users');
 
-requestStream.subscrite((requestUrl) => {
-  jQuery.getJson(requestUrl, (resopnseData) => {
+requestStream.subscribe((requestUrl) => {
+  let responseStream = Rx.Observable.create((observer) => {
+    $.getJSON(requestUrl)
+      .done((response) => {
+        observer.onNext(response);
+      })
+      .fail((jqXHR, status, error) => {
+        observer.onError(error);
+      })
+      .always(() => {
+        observer.onCompleted();
+      });
+  });
+
+  responseStream.subscribe((response) => {
 
   });
+
 })
